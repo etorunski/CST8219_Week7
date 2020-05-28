ce506abde7a1bd6431177a517ef0faa935045254
Eric Torunski <etorunski@gmail.com>
2020-05-28 14:32:47 -0400

    Initial Commit

---
+++ week4.cpp
@@ -0,0 +1,41 @@
+﻿// week4.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Fraction {
+	int numerator, denominator;
+
+public: 
+	Fraction(int n, int d) {
+		setNumerator(n);
+		setDenominator(d);
+	}
+
+	void setNumerator(int n){ numerator = n;	}
+	int getNumerator(void) {  return numerator;  }
+
+	void setDenominator(int d) { denominator = d; }
+	int getDenominator(void) { return denominator; }
+
+	float asFloat(void) {
+		return (float)getNumerator() / getDenominator();
+	}
+};
+
+void printFraction(Fraction f)
+{
+	cout << "Fraction at address:" << &f
+		<< " Numerator:" << f.getNumerator()
+		<< " Denominator:" << f.getDenominator() << endl;
+}
+
+int main()
+{
+	Fraction f(5, 1);
+	printFraction(f);
+
+	return 0;
+}
